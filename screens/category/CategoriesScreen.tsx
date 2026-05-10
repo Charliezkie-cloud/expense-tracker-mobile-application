bdd9e3c74fd1b9db3632cc2dfa543e2559946e9f
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-05-10 13:10:01 +0800

    Added sorting to the categories

--- screens/category/CategoriesScreen.tsx
+++ screens/category/CategoriesScreen.tsx
@@ -1,16 +1,17 @@
 import { FlatList, StyleSheet, View } from "react-native";
-import { Button, List, Text } from "react-native-paper";
+import { Button, List, Modal, Portal, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { ChevronRight, Plus } from "lucide-react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ArrowUpDown, ChevronRight, Plus } from "lucide-react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useEffect, useState } from "react";
 
 import { containers } from "../../styles/containers";
 import { RootParamStackList } from "../../types/navigation.types";
 import { useCategoryStore } from "../../hooks/useCategoryStore";
 import { Category } from "../../types/data.types"
 import { convertDateToDateString } from "../../utils/converters";
-import { useEffect } from "react";
+import { sortCategories } from "../../utils/sorters";
 
 type NavProps = NativeStackNavigationProp<RootParamStackList, "AddCategory">;
 
@@ -19,21 +20,111 @@ export default function CategoriesScreen() {
   const navigation = useNavigation<NavProps>();
   const categories = useCategoryStore((state) => state.categories);
 
+  // States
+  const [filteredCategories, setFilteredCategories] = useState<Category[]>([]);
+
+  const [sortingModal, setSortingModal] = useState(false);
+
+  const [sortBySelectedItem, setSortBySelectedItem] = useState<"createdAt" | "updatedAt">("createdAt");
+  const [sortOrderSelectedItem, setSortOrderSelectedItem] = useState<"ascending" | "descending">("ascending");
+
   // Handlers
   function addCategoryButtonOnPress() {
     navigation.navigate("AddCategory");
+  }
+
+  function toggleSortingModal() {
+    setSortingModal(prev => !prev);
+  }
+
+  function applySortingButtonOnPress() {
+    const sortedCategories = sortCategories(categories, sortBySelectedItem, sortOrderSelectedItem);
+    setFilteredCategories(sortedCategories);
+    toggleSortingModal();
   }
 
   function listItemButtonOnPress(category: Category) {
     navigation.navigate("Category", category)
   }
 
+  // Use effects
+  useEffect(() => {
+    const sortedCategories = sortCategories(categories, sortBySelectedItem, sortOrderSelectedItem);
+    setFilteredCategories(sortedCategories);
+  }, [categories]);
+
   return (
     <View style={styles.mainContainer}>
 
-      {/* List */}
+      {/* Sort button */}
+      <Button
+        labelStyle={{ fontSize: 16 }}
+        icon={(props) => (
+          <ArrowUpDown color={props.color} size={props.size} />
+        )}
+        onPress={toggleSortingModal}
+      >
+        Sort Categories
+      </Button>
+
+      <Portal>
+        <Modal visible={sortingModal} style={{ margin: 24 }}>
+          <View
+            style={{
+              gap: 14,
+              padding: 24,
+              backgroundColor: "#e5e7eb",
+              borderRadius: 8
+            }}
+          >
+            <Text variant="headlineSmall">Category Sorting</Text>
+
+            <View style={{ gap: 8 }}>
+              <Text variant="bodyLarge">Sort by</Text>
+              <Picker
+                style={{ backgroundColor: "#d1d5db" }}
+                selectedValue={sortBySelectedItem}
+                onValueChange={(e) => setSortBySelectedItem(e)}
+              >
+                <Picker.Item label="Creation Date & Time" value="createdAt" />
+                <Picker.Item label="Updation Date & Time" value="updatedAt" />
+              </Picker>
+            </View>
+
+            <View style={{ gap: 8 }}>
+              <Text variant="bodyLarge">Order</Text>
+              <Picker
+                style={{ backgroundColor: "#d1d5db" }}
+                selectedValue={sortOrderSelectedItem}
+                onValueChange={(e) => setSortOrderSelectedItem(e)}
+              >
+                <Picker.Item label="Ascending" value="ascending" />
+                <Picker.Item label="Descending" value="descending" />
+              </Picker>
+            </View>
+
+            <View style={{ gap: 8 }}>
+              <Button
+                mode="contained"
+                labelStyle={{ fontSize: 16 }}
+                onPress={applySortingButtonOnPress}
+              >
+                Apply
+              </Button>
+              <Button
+                labelStyle={{ fontSize: 16 }}
+                onPress={toggleSortingModal}
+              >
+                Close
+              </Button>
+            </View>
+          </View>
+        </Modal>
+      </Portal>
+
+      {/* Category list */}
       <FlatList
-        data={categories}
+        data={filteredCategories}
         renderItem={(({ item }) => (
           <List.Item
             title={item.name}
--- utils/sorters.ts
+++ utils/sorters.ts
@@ -1,7 +1,7 @@
-import { Expense } from "../types/data.types";
+import { Category, Expense } from "../types/data.types";
 
 /**
- * Sorts the expenses object for ya ;D
+ * Sorts the expenses array for ya ;D
  * @param expenses The array of expenses object
  * @param property The propert you want to sort
  * @param order The order of the sort
@@ -20,7 +20,30 @@ export function sortExpenses(
     if (property === "price" || property === "quantity")
       result = a[property] - b[property];
     else
-      result = a[property].localeCompare(b[property]);
+      result = new Date(a[property]).getTime() - new Date(b[property]).getTime();
+
+    return order === "ascending" ? result : -result;
+  });
+}
+
+/**
+ * Sorts the categories array for ya ;D
+ * @param categories An array of category object
+ * @param property The property you want to sort by
+ * @param order The order of the sort
+ * @returns The sorted array of category object
+ */
+export function sortCategories(
+  categories: Category[],
+  property: "createdAt" | "updatedAt",
+  order: "ascending" | "descending"
+) {
+  const sorted = [...categories];
+
+  return sorted.sort((a, b) => {
+    let result = 0;
+
+    result = new Date(a[property]).getTime() - new Date(b[property]).getTime();
 
     return order === "ascending" ? result : -result;
   });
